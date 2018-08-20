4ba1c4a6fbd95aa09fb0b533a4446418d213ee4b
Asif Bacchus <asif@noreply.example.org>
2018-08-19 19:17:02 -0600

    Delete 'bash/etc-profile.d scripts/commandAliases.sh'
    No longer used.

--- bash/etc-profile.d scripts/commandAliases.sh
+++
@@ -1,9 +0,0 @@
-### Note: If you change the name of this file, it must come AFTER colourLS.sh
-### alphabetically to avoid errors from references to LS_OPTIONS
-
-### Set command aliases
-alias ll='ls $LS_OPTIONS -l'
-alias l='ls $LS_OPTIONS -lA'
-alias rm='rm -i'
-alias mv='mv -i'
-alias cp='cp -i'
